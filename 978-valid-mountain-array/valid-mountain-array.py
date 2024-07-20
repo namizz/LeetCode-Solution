308599c53bc90ef4d0579a449cd1e5806bc80226
namizz <139370392+namizz@users.noreply.github.com>
2024-07-21 00:24:06 +0300

    Time: 149 ms (92.00%) | Memory: 18.1 MB (29.65%) - LeetSync

--- 978-valid-mountain-array/valid-mountain-array.py
+++ 978-valid-mountain-array/valid-mountain-array.py
@@ -1,25 +1,22 @@
 class Solution:
     def validMountainArray(self, arr: List[int]) -> bool:
         j = 0
-        if len(arr) < 2:
-            return False
+        increase = False
+        decrease = False
         for i in range(len(arr)-1):
             if arr[i] < arr[i+1]:
                 j += 1
-            
+                increase = True
             elif arr[i] == arr[i+1] or arr[0] > arr[1]:
                 return False
             else:
                 break
-        if j == len(arr)-1:
-            return False
         for i in range(j,len(arr)-1):
             if arr[i] > arr[i+1]:
                 j+=1
+                decrease = True
             else:
                 return False
-        if j == len(arr) - 1:
-            return True
-        return False
+        return increase and decrease
             
         
