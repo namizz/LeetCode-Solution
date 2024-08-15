07449792d1585c9593d714c57e8610315f915262
namizz <139370392+namizz@users.noreply.github.com>
2024-08-15 09:25:25 +0300

    Time: 30 ms (87.67%) | Memory: 16.5 MB (47.00%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,36 +1,17 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        order = 0
-        check = 0
-        result = 0
-        x = 0
-        y = 0
-        z =0
-        for i in s:
-            if i == '[' or i == '{' or i == '(':
-                order += 1
-                check += 1
-                if i == '(':
-                    x += order
-                elif i == '{':
-                    y += order
+        stack = []
+        for i in range(len(s)):
+            if s[i] == '(' or s[i] == '[' or s[i]== '{':
+                stack.append(s[i])
+            else:
+                if stack and ((s[i] == ')' and stack.pop() == '(') or (s[i]== ']' and stack.pop()=='[') or (s[i]== '}'and stack.pop() == '{')):
+                    continue
                 else:
-                    z += order
-            elif i == ']' or i == '}' or i == ')':
-                check -= 1
-                if i == ')':
-                    x -= order
-                elif i == '}':
-                    y -= order
-                else:
-                    z -= order
-                order -= 1
-                print(x,y,z)
-            if (check < 0) or (x < 0) or (y < 0) or (z < 0):
-                return False
-                break
-
-        if x == 0 and y ==0 and z == 0 :
-            return True
-
+                    return False
+        if stack:
+            return False
+        return True
+                
+        
 
