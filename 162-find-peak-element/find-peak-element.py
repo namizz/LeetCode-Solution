d20a2f606196033180cd30bbbb70c1172c3369fd
Naomi Zerfu <139370392+namizz@users.noreply.github.com>
2024-10-10 05:47:05 -0700

    Time: 42 ms (82.10%) | Memory: 16.8 MB (27.14%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -1,22 +1,17 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
-        for i in range(len(nums)-1):
-            if nums[i] > nums[i+1]:
-                return i
-        return len(nums) - 1
-        # lp = 0
-        # rp = len(nums) - 1
-        # while lp < rp-1:
-        #     mid = (lp + rp)//2
-        #     print(lp, mid, rp)
-        #     if nums[lp] < nums[mid] and nums[mid] < nums[rp]:
-        #         lp += 1
-        #         if nums[lp-1] > nums[lp]:
-        #             return lp-1
-        #     else:
-        #         if nums[lp] > nums[mid]:
-        #             rp = mid
-        #         else:
-        #             lp = mid
-        # return lp if len(nums) > 1 and nums[lp] > nums[lp+1] else len(nums) - 1
+        if len(nums) == 1:  return 0
+        if nums[0] > nums[1]:  return 0
+        if nums[-1] > nums[-2]:  return len(nums)-1
+        
+        lp, rp = 0, len(nums) - 1
+        while lp+1 < rp:
+            mid = (lp + rp)//2
+            if nums[mid] > nums[mid+1] and nums[mid] > nums[mid-1]:
+                return mid
+            if nums[mid-1] < nums[mid]:
+                lp = mid + 1
+            else:
+                rp = mid - 1
+        return lp if nums[lp] > nums[rp] else rp
         
