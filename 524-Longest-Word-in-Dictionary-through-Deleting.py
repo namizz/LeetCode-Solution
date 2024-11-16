1aa460d4978ddfe47efd5b54c0d1fb63ffd54b49
Naomi Zerfu <139370392+namizz@users.noreply.github.com>
2024-11-16 19:04:06 +0300

    [524] [Time Beats: 5.54%] [Memory Beats: 89.60%] - LeetPush

---
+++ 524-Longest-Word-in-Dictionary-through-Deleting.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findLongestWord(self, s: str, dictionary: List[str]) -> str:
+        
+        #["ale","apple","monkey","plea"]
+        ans = ""
+        #aabcelpp , aelpp, ans="ale", j,k= 0,0
+        for word in dictionary: 
+            j = 0   
+            k = 0
+            while j < len(word) and k < len(s):
+                if word[j] == s[k]:
+                    j += 1
+                k += 1
+                if j == len(word):
+                    if len(word) > len(ans):
+                        ans = word
+                    elif len(word) == len(ans):
+                        ans = min(ans, word)
+                    else:
+                        break
+        return ans
+
+
+                
+                
+
+        
