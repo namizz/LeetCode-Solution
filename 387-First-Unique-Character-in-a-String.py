0924b7a8b0007adeb97a00533bbff9e499465995
Naomi Zerfu <139370392+namizz@users.noreply.github.com>
2024-12-16 14:13:29 +0300

    [387] [Time Beats: 85.47%] [Memory Beats: 8.87%] - LeetPush

---
+++ 387-First-Unique-Character-in-a-String.py
@@ -0,0 +1,9 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        hashmap = Counter(s)
+        for i in range(len(s)):
+            if hashmap[s[i]] == 1:
+                return i
+        return -1
+
+        
