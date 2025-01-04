bf7cc4ad2b53fa7a1228a323b35c9c41b6659894
Naomi Zerfu <139370392+namizz@users.noreply.github.com>
2025-01-05 01:06:29 +0300

    [111] [Time Beats: 100.00%] [Memory Beats: 35.41%] - LeetPush

--- 111-Minimum-Depth-of-Binary-Tree.py
+++ 111-Minimum-Depth-of-Binary-Tree.py
@@ -8,13 +8,16 @@ class Solution:
     def minDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
-        def dfs(node):
-            if not node:
-                return 0
-            if node.left and node.right:
-                return 1+min(dfs(node.left), dfs(node.right))
-            elif node.left:
-                return 1+dfs(node.left)
-            else:
-                return 1+dfs(node.right)
-        return dfs(root)
+        queue = deque([root])
+        ans = 0
+        while queue:
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                if not node.right and not node.left:
+                    return ans + 1
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            ans += 1
+                
