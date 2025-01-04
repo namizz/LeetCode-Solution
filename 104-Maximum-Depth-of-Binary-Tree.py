aa120fe09b946250561de2ac8f37fc6e21d2dffb
Naomi Zerfu <139370392+namizz@users.noreply.github.com>
2025-01-05 00:58:03 +0300

    [104] [Time Beats: 100.00%] [Memory Beats: 26.68%] - LeetPush

--- 104-Maximum-Depth-of-Binary-Tree.py
+++ 104-Maximum-Depth-of-Binary-Tree.py
@@ -8,15 +8,11 @@ class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
-        queue = deque([root])
-        ans = 0
-        while queue:
-            for _ in range(len(queue)):
-                node = queue.popleft()
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
-            ans += 1
-        return ans
+        def dfs(node):
+            global depth
+            if not node:
+                return  0
+            return 1+max(dfs(node.left), dfs(node.right))
+        return dfs(root)
+
         
