320c30ba2e52589f607d853dfcabe0d83b266778
Naomi Zerfu <139370392+namizz@users.noreply.github.com>
2025-01-14 13:32:42 +0300

    [2657] [Time Beats: 5.21%] [Memory Beats: 9.90%] - LeetPush

---
+++ DCP-01-25/2657-Find-the-Prefix-Common-Array-of-Two-Arrays.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findThePrefixCommonArray(self, A: List[int], B: List[int]) -> List[int]:
+        # A=[], B=[]
+        # return C=[] where 
+        # C[i] = number present before or at A[i] and B[i]
+        # { 1:0, 3:1, 2:2, 4:3}
+        # hashamp[3] >= 0:
+        # hashmap[1] >= 1 , 1
+        # hashmap[2] >=2 , 2
+        # hashmap[4], 3
+        #[0,2,3,4]
+        hashmap = {}
+        for i in range(len(A)):
+            hashmap[A[i]] = i
+        count = 0
+        ans = [0]*(len(A))
+        for i in range(len(B)):
+            if hashmap[B[i]] <= i:
+                count += 1
+            else:
+                ans[hashmap[B[i]]] += 1
+            ans[i] += count
+            count = ans[i]
+            print(count, ans, hashmap)
+        return ans
+
+        
