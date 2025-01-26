11e953bdc065d32640d4f09f7455c2747e30fea6
Naomi Zerfu <139370392+namizz@users.noreply.github.com>
2025-01-26 09:15:20 +0300

    Time: 19 ms (55.9%), Space: 19.4 MB (99.22%) - LeetHub

--- 1267-count-servers-that-communicate/1267-count-servers-that-communicate.py
+++ 1267-count-servers-that-communicate/1267-count-servers-that-communicate.py
@@ -1,25 +1,22 @@
 class Solution:
     def countServers(self, grid: List[List[int]]) -> int:
-        if not grid:
-            return 0
-        rows = [0] * len(grid[0])
-        cols = [0] * len(grid)
-
-        # Count servers in each row and each column
+        # grid = [[]] :List->m*n
+        # two servers communicate with each other if they are on the same row or col
+        # return number of servers that communicate
+        # number of communicating server cannot be greater than the total number of the server
+        # count the row if they have more than 1 server
+        # count the col if they have more than 1 server
+        row = [0] * len(grid)
+        col = [0] * len(grid[0])
         for r in range(len(grid)):
             for c in range(len(grid[0])):
-                if grid[r][c]:
-                    rows[c] += 1
-                    cols[r] += 1
-
+                if grid[r][c] == 1:
+                    row[r] += 1
+                    col[c] += 1
         ans = 0
-
-        # Count servers that can communicate (in the same row or column)
         for r in range(len(grid)):
             for c in range(len(grid[0])):
-                if grid[r][c]:
-                    if rows[c] > 1 or cols[r] > 1:
-                        ans += 1
-
+                if grid[r][c] == 1 and(row[r] > 1 or col[c] > 1):
+                    ans += 1
         return ans
         
