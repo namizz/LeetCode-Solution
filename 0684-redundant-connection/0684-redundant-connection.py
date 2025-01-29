3852000646ae1324f3f2868b56f3ee99817f08f6
Naomi Zerfu <139370392+namizz@users.noreply.github.com>
2025-01-29 22:27:32 +0300

    Time: 9 ms (21.67%), Space: 18.3 MB (34.45%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -0,0 +1,33 @@
+class Solution:
+    def _is_connected(self, src, target, visited, adj_list):
+        visited[src] = True
+
+        if src == target:
+            return True
+
+        is_found = False
+        for adj in adj_list[src]:
+            if not visited[adj]:
+                is_found = is_found or self._is_connected(
+                    adj, target, visited, adj_list
+                )
+
+        return is_found
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        #dfp when visted node is repeted remove the edge
+        N = len(edges)
+
+        adj_list = [[] for _ in range(N)]
+
+        for edge in edges:
+            visited = [False] * N
+
+            # If DFS returns True, we will return the edge.
+            if self._is_connected(edge[0] - 1, edge[1] - 1, visited, adj_list):
+                return edge
+
+            adj_list[edge[0] - 1].append(edge[1] - 1)
+            adj_list[edge[1] - 1].append(edge[0] - 1)
+
+        return []
+            
