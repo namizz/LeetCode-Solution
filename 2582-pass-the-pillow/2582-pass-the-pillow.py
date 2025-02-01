b4662ba5213f7291c38fc1d3bffb264253fede5c
Naomi Zerfu <139370392+namizz@users.noreply.github.com>
2025-02-01 08:11:08 +0300

    Time: 0 ms (100%), Space: 17.7 MB (35.23%) - LeetHub

--- 2582-pass-the-pillow/2582-pass-the-pillow.py
+++ 2582-pass-the-pillow/2582-pass-the-pillow.py
@@ -1,10 +1,4 @@
 class Solution:
     def passThePillow(self, n: int, time: int) -> int:
-        temp = n - 1
-        front = True
-        while temp < time:
-            time -= temp
-            front = not(front)
-        if front:
-            return time + 1
-        return n - time
+        return time%(n-1)+1 if (time//(n-1))%2 == 0 else n - (time%(n-1))
+        
