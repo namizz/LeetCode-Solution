18590b713bd0cc895366ae85da7b6fedcb2bae84
Naomi Zerfu <139370392+namizz@users.noreply.github.com>
2025-02-05 16:56:40 +0300

    Time: 3 ms (80.09%), Space: 18 MB (5.47%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -1,16 +1,15 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        hashmap = {1:"I",4:"IV", 5:"V",9:"IX", 10:"X",40:"XL", 50:"L",90:"XC", 100:"C",400:"CD", 500:"D",900:"CM", 1000:"M"}
+        hashmap = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
         arr = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
         ans = []
         i = 0
         while num > 0:
             if num >= arr[i]:
-                ans.append(hashmap[arr[i]])
+                ans.append(hashmap[i])
                 num -= arr[i]
             else:
                 i += 1
-        print(ans)
         return "".join(ans)
 
         
