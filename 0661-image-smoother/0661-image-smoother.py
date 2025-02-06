8946a42c49cc1ed03cd90e81a9c71f2c23dac8d4
Naomi Zerfu <139370392+namizz@users.noreply.github.com>
2025-02-06 18:59:32 +0300

    Time: 159 ms (66.05%), Space: 19 MB (13.29%) - LeetHub

--- 0661-image-smoother/0661-image-smoother.py
+++ 0661-image-smoother/0661-image-smoother.py
@@ -1,23 +1,32 @@
 class Solution:
     def imageSmoother(self, img: List[List[int]]) -> List[List[int]]:
-        direction = [(-1,-1),(-1,0),(0,-1),(1,0),(0,1),(1,1),(-1,1),(1,-1)]
+        direction = [
+            (-1, -1),
+            (-1, 0),
+            (0, -1),
+            (1, 0),
+            (0, 1),
+            (1, 1),
+            (-1, 1),
+            (1, -1),
+        ]
         row = len(img)
         col = len(img[0])
-        matrix = [[0]*len(img[0]) for _ in range(len(img))]
+        # after computing make the average*256+number and if the number > 256 make a %256 an find the number
         for r in range(row):
             for c in range(col):
-                total = img[r][c]
+                total = img[r][c]%256
                 count = 1
-                for dr,dc in direction:
-                    rr = dr+r
-                    cc = dc+c
+                for dr, dc in direction:
+                    rr = dr + r
+                    cc = dc + c
                     if 0 <= rr < row and 0 <= cc < col:
+                        total += (img[rr][cc] % 256)
                         count += 1
-                        total += img[rr][cc]  
-                matrix[r][c] = total//count
-        return matrix
+                total = total//count
+                img[r][c] = (total*256 + img[r][c])
+        for r in range(row):
+            for c in range(col):
+                img[r][c] //= 256
+        return img
 
-                    
-
-
-        
