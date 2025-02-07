b213b03614091567fdc1722288303df94038209f
Naomi Zerfu <139370392+namizz@users.noreply.github.com>
2025-02-07 10:12:35 +0300

    Time: 1 ms (23.93%), Space: 18 MB (15.58%) - LeetHub

--- 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.py
+++ 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.py
@@ -6,22 +6,21 @@ class Solution:
         #  [3,4,5],
         #  [6,7,8]]
         n = len(mat)
+
         def rotate_90(mat):
-            transpose = [[0]*n for _ in range(n)]
             for i in range(n):
-                for j in range(n):
-                    transpose[j][i] = mat[i][j]
+                for j in range(i):
+                    mat[j][i], mat[i][j] = mat[i][j], mat[j][i]
             for i in range(n):
                 l = 0
-                r = n-1
+                r = n - 1
                 while l < r:
-                    transpose[i][l], transpose[i][r] = transpose[i][r], transpose[i][l]
+                    mat[i][l], mat[i][r] = mat[i][r], mat[i][l]
                     l += 1
                     r -= 1
-            return transpose
-        for i in range(n):
+            return mat
+
+        for _ in range(4):
             if rotate_90(mat) == target:
                 return True
-            else:
-                mat = rotate_90(mat)
         return False
