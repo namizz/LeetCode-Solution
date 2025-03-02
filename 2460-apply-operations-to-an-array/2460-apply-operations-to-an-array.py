1d1b1ce2a01c9132c69d292bf0bc06ba33d535cd
Naomi Zerfu <139370392+namizz@users.noreply.github.com>
2025-03-02 22:39:36 +0300

    Time: 0 ms (100%), Space: 17.9 MB (74.69%) - LeetHub

--- 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.py
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.py
@@ -3,8 +3,8 @@ class Solution:
         ans = []
         i = 0
         while i < len(nums):
-            if i+1 < len(nums) and nums[i]== nums[i+1]:
-                ans.append(nums[i]*2)
+            if i + 1 < len(nums) and nums[i] == nums[i + 1]:
+                ans.append(nums[i] * 2)
                 ans.append(0)
                 i += 1
             else:
@@ -16,5 +16,5 @@ class Solution:
                 zeros = i
             elif ans[zeros] == 0 and ans[i] != 0:
                 ans[i], ans[zeros] = ans[zeros], ans[i]
-                zeros +=1
+                zeros += 1
         return ans
