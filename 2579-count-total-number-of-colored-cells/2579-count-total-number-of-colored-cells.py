5c14fe675bea7c87787242a6d72f2b7827958eb7
Naomi Zerfu <139370392+namizz@users.noreply.github.com>
2025-03-05 18:34:25 +0300

    Time: 0 ms (100%), Space: 17.9 MB (30.45%) - LeetHub

--- 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.py
+++ 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.py
@@ -1,15 +1,5 @@
 class Solution:
     def coloredCells(self, n: int) -> int:
-        if n == 1:
-            return 1
-        ans = 1
-        ans += (((n-2)*(n-1))//2)*4
-        ans += 4*(n-2)
-        return ans + 4
+        return 5 + ((((n-2)*(n-1))//2) + (n-2))*4 if n != 1 else 1
 
-
-
-
-        
-        return pow(horz-2,2)+4
         
