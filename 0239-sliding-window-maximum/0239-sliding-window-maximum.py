46bb56acb68ff89e493c5e3d3a4981d2e484d625
Naomi Zerfu <139370392+namizz@users.noreply.github.com>
2025-03-08 08:13:50 +0300

    Time: 123 ms (99.02%), Space: 31.1 MB (79.44%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -7,15 +7,14 @@ class Solution:
                 while decr and nums[i] > decr[-1]:
                     decr.pop()
                 decr.append(nums[i])
-        if decr:
-            ans.append(decr[0])
+        ans.append(decr[0])
         for i in range(k, len(nums)):
+            if decr and decr[0] == nums[i-k]:
+                decr.popleft()
             while decr and nums[i] > decr[-1]:
                 decr.pop()
             decr.append(nums[i])
             ans.append(decr[0])
-            if decr and decr[0] == nums[i-k]:
-                decr.popleft()
             
         return ans
             
