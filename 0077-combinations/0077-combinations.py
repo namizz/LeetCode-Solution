dd68c435eed051172c7197b8138d83d50a5095eb
Naomi Zerfu <139370392+namizz@users.noreply.github.com>
2025-03-20 18:44:15 +0300

    Time: 190 ms (20.14%), Space: 59.6 MB (57.92%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,15 +1,17 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         ans,path = [], []
-        def backtrack(start):
-            if len(path) == k:
-                ans.append(path.copy())
+        def backtrack(i,combination):
+            if i > n:
+                if len(combination) == k:
+                    ans.append(combination[:])
                 return
-            for i in range(start+1,n+1):
-                path.append(i)
-                backtrack(i)
-                path.pop()
-        backtrack(0)
+            combination.append(i)
+            backtrack(i+1,combination)
+            combination.pop()
+            backtrack(i+1,combination)
+
+        backtrack(1,[])
         return ans
 
         
