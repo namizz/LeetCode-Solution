79c0f6fed2d4050164102010fe55c26d6b474f78
Naomi Zerfu <139370392+namizz@users.noreply.github.com>
2025-03-27 18:45:26 +0300

    Time: 981 ms (33.47%), Space: 22 MB (51.48%) - LeetHub

--- 2594-minimum-time-to-repair-cars/2594-minimum-time-to-repair-cars.py
+++ 2594-minimum-time-to-repair-cars/2594-minimum-time-to-repair-cars.py
@@ -1,20 +1,20 @@
 class Solution:
     def repairCars(self, ranks: List[int], cars: int) -> int:
-        freq=Counter(ranks)
-        minR=min(ranks)
-        def canRepair(t):
-            cnt=0
-            for x, f in freq.items():
-                cnt+=f*(sqrt(t/x)//1)
-                if cnt>=cars: return True
-            return cnt>=cars
-        
-        l, r=1, minR*cars*cars
-        while l<r:
-            m=(l+r)>>1
-            if canRepair(m):
-                r=m
+        l,r = 1,max(ranks)*(cars**2)
+        def check(t):
+            count = 0
+            for i in range(len(ranks)):
+                count += int(sqrt(t/ranks[i]))
+            return count >= cars
+        ans = 0
+        while l <= r:
+            mid = (l+r)//2
+            if check(mid):
+                ans = mid
+                r = mid - 1
             else:
-                l=m+1
-        return l
+                l = mid + 1
+        return ans
+
+        
         
