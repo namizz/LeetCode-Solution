00d60d91d0b2f2abaf39032a7a6295a2a78cde5d
Naomi Zerfu <139370392+namizz@users.noreply.github.com>
2025-03-27 10:29:16 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3211-generate-binary-strings-without-adjacent-zeros/3211-generate-binary-strings-without-adjacent-zeros.py
@@ -0,0 +1,17 @@
+class Solution:
+    def validStrings(self, n: int) -> List[str]:
+        ans = []
+        def back(path):
+            if len(path) == n:
+                ans.append("".join(path))
+                return
+            print(path)
+            back(path+['0'])
+            back(path+['1'])
+        back([])
+        res = []
+        for i in ans:
+            if "00" not in i:
+                res.append(i)
+        return res
+        
