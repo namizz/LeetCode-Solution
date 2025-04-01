9be68c08a6e1f3596910c788d9b60234dd9c714f
Naomi Zerfu <139370392+namizz@users.noreply.github.com>
2025-04-01 10:56:48 +0300

    Time: 339 ms (69.22%), Space: 29.7 MB (47.91%) - LeetHub

--- 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.py
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.py
@@ -1,35 +1,18 @@
 class Solution:
     def maximumCandies(self, candies, k):
-        # Find the maximum number of candies in any pile
-        max_candies_in_pile = 0
-        for candy in candies:
-            max_candies_in_pile = max(max_candies_in_pile, candy)
+        def check(x):
+            pos = 0
+            for i in candies:
+                pos += (i//x)
+            return pos >= k
 
-        # Set the initial search range for binary search
-        left = 0
-        right = max_candies_in_pile
-
-        # Binary search to find the maximum number of candies each child can get
-        while left < right:
-            # Calculate the middle value of the current range
-            middle = (left + right + 1) // 2
-
-            # Check if it's possible to allocate candies so that each child gets 'middle' candies
-            if self._can_allocate_candies(candies, k, middle):
-                # If possible, move to the upper half to search for a larger number
-                left = middle
+        l, r = 1, max(candies)
+        ans = 0
+        while l <= r:
+            mid = (l+r)//2
+            if check(mid):
+                ans = mid
+                l = mid + 1
             else:
-                # Otherwise, move to the lower half
-                right = middle - 1
-
-        return left
-
-    def _can_allocate_candies(self, candies, k, num_of_candies):
-        # Initialize the total number of children that can be served
-        max_num_of_children = 0
-
-        # Iterate over all piles to calculate how many children each pile can serve
-        for pile in candies:
-            max_num_of_children += pile // num_of_candies
-
-        return max_num_of_children >= k
+                r = mid - 1
+        return ans
