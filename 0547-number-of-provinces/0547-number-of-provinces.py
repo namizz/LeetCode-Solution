313e29ebabe917ce73149ce6b2c6725ada138f12
Naomi Zerfu <139370392+namizz@users.noreply.github.com>
2025-04-15 10:27:57 +0300

    Time: 6 ms (61.92%), Space: 19 MB (56.06%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -5,9 +5,13 @@ class Solution:
         ans = 0
         for r in range(len(isConnected)):
             if r not in ingroup:
-                for c in range(len(isConnected[0])):
-                    if isConnected[r][c]:
-                        ingroup.add(c)
+                queue = deque([r])
+                while queue:
+                    x = queue.popleft()
+                    for c in range(len(isConnected[0])):
+                        if isConnected[x][c] and c not in ingroup:
+                            ingroup.add(c)
+                            queue.append(c)
                 ans += 1
         return ans
 
