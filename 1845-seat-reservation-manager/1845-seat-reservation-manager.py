b7429f08703711c1cb2d37b1e1d539e7ff17dcf0
Naomi Zerfu <139370392+namizz@users.noreply.github.com>
2025-05-06 01:41:13 +0300

    Time: 187 ms (20.81%), Space: 47 MB (40.64%) - LeetHub

--- 1845-seat-reservation-manager/1845-seat-reservation-manager.py
+++ 1845-seat-reservation-manager/1845-seat-reservation-manager.py
@@ -2,21 +2,16 @@ class SeatManager:
 
     def __init__(self, n: int):
         self.h = []
-        self.contain = set()
         for i in range(n):
             heapq.heappush(self.h, i+1)    
-            self.contain.add(i+1)    
 
     def reserve(self) -> int:
         x = heapq.heappop(self.h)
-        self.contain.remove(x)
         return x
         
 
     def unreserve(self, seatNumber: int) -> None:
-        if seatNumber not in self.contain:
-            heapq.heappush(self.h, seatNumber)
-            self.contain.add(seatNumber)
+        heapq.heappush(self.h, seatNumber)
 
         
         
