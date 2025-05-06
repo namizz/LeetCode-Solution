21f5ea7249cbb7732555d23cbd9477dd4252ff06
Naomi Zerfu <139370392+namizz@users.noreply.github.com>
2025-05-06 09:47:15 +0300

    Time: 43 ms (63.8%), Space: 20.9 MB (13.22%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.py
+++ 1462-course-schedule-iv/1462-course-schedule-iv.py
@@ -2,26 +2,31 @@ class Solution:
     def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
         # numCourse: int, prerequisites= [[]], queries = [[]]
         # true if queries[j] =[ui, vi] where ui is prerequsite for vi
-        # check if quries are true or false
-        # make a storage which could be easily accessed by the querie where when you inter vi it check if ui is one of the prerequiste or no
-        # make a 2d array based on the course number and make 1 if the course is dependent
-        # else make 0
-        # this is called adjecent matrix
-        # [1,0,1]
-        # [0,0,0]
-        # [0,1,0]
-        # 
-        arr = [[0]*numCourses for i in range(numCourses)]
-        for c,r in prerequisites:
-            arr[r][c] = 1
-        print(arr)
+        
+        graph = defaultdict(set)
+        indeg = defaultdict(int)
+        for a,b in prerequisites:
+            graph[a].add(b)
+            indeg[b] += 1
+        # indirect relation
         queue = deque()
-        ans = []
         for i in range(numCourses):
-            for j in range(numCourses):
-                for k in range(numCourses):
-                    if arr[i][j] and arr[k][i]:
-                        arr[k][j] = 1
-        for ui, vi in queries:
-            ans.append(arr[vi][ui] == 1)  
-        return ans
+            if indeg[i] == 0:
+                queue.append(i)
+
+        ans = defaultdict(set)
+        while queue:
+            for i in range(len(queue)):
+                x = queue.popleft()
+                for i in graph[x]:
+                    ans[i].add(x)
+                    ans[i].update(ans[x])
+                    indeg[i] -= 1
+                    if indeg[i] == 0:
+                        queue.append(i)
+        # print(ans)
+        result = []
+        for ui,vi in queries:
+            result.append(ui in ans[vi])
+        return result
+             
