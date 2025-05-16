86ce87ba444eb25bb349f1ed388878332b60f476
Naomi Zerfu <139370392+namizz@users.noreply.github.com>
2025-05-16 11:56:49 +0300

    Time: 88 ms (71.9%), Space: 31.9 MB (73.11%) - LeetHub

--- 2401-longest-nice-subarray/2401-longest-nice-subarray.py
+++ 2401-longest-nice-subarray/2401-longest-nice-subarray.py
@@ -1,15 +1,13 @@
 class Solution:
     def longestNiceSubarray(self, nums):
-        l = 0
-        usedBits = 0
-        maxLength = 0
-
-        for r in range(len(nums)):
-            while (usedBits & nums[r]) != 0:
-                usedBits ^= nums[l]
-                l += 1
-
-            usedBits |= nums[r]
-            maxLength = max(maxLength, r - l + 1)
-
-        return maxLength
+        ans = 0
+        lp = 0
+        bt = 0
+        for rp in range(len(nums)):
+            while bt & nums[rp]:
+                bt ^= nums[lp]
+                lp += 1
+            bt |= nums[rp]
+            ans = max(ans, rp - lp + 1)
+        return ans
+        
