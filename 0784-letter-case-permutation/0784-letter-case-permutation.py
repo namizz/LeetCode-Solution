7a7dba3b9153a921e1e662cea30e8f9f8378b630
Naomi Zerfu <139370392+namizz@users.noreply.github.com>
2025-05-23 19:09:11 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0784-letter-case-permutation/0784-letter-case-permutation.py
@@ -0,0 +1,38 @@
+class Solution:
+    def letterCasePermutation(self, s: str) -> List[str]:
+        alpha = []
+        for i in range(len(s)):
+            if not s[i].isdigit():
+                alpha.append(i)
+        # print(alpha)
+        ans = []
+        for i in range(2**(len(alpha))):
+            # print(i, bin(i))
+            k = i
+            pos = []
+            for j in range(len(alpha)):
+                upto = alpha[j]
+                if not j:
+                    pos.extend(s[:upto])
+                else:
+                    frm = alpha[j-1]+1
+                    pos.extend(s[frm: upto])
+                #change the letter
+                if k&1:
+                    if s[alpha[j]].islower():
+                        pos.append(s[alpha[j]].upper())
+                    else:
+                        pos.append(s[alpha[j]].lower())
+
+                
+                #don't change the letter
+                else:
+                    pos.append(s[alpha[j]])
+
+                k >>= 1
+            pos.extend(s[alpha[-1]+1:])
+            # print(pos)
+            ans.append("".join(pos))
+        return ans
+
+        
