9ef32c8f135affb7d5e094c3427ee018edd92bf3
Naomi Zerfu <139370392+namizz@users.noreply.github.com>
2025-06-14 13:38:12 +0300

    Time: 1337 ms (16.24%), Space: 60.7 MB (94.65%) - LeetHub

--- 3356-zero-array-transformation-ii/3356-zero-array-transformation-ii.py
+++ 3356-zero-array-transformation-ii/3356-zero-array-transformation-ii.py
@@ -1,25 +1,30 @@
 class Solution:
     def minZeroArray(self, nums: List[int], queries: List[List[int]]) -> int:
-        # queries [l,r,v]--> decrease by value v from nums[l] to nums[r]
-        # 
-        _max = max(nums)
-        j = 0
-        while _max > 0:
-            prefix = [0]*(len(nums)+1)
-            temp = 0
-            while temp < _max and j < len(queries):
-                l,r,v = queries[j]
-                temp += v
-                prefix[l] -= v
-                prefix[r+1] += v
-                j += 1
-            if j == len(queries):
-                return -1
-            for i in range(1,len(prefix)):
-                prefix[i] += prefix[i-1]
-            _max = float(-inf)
+        l= 0
+        r = len(queries)
+        def valid(mid):
+            temp = [0]*len(nums) +[0]
+            for i in range(mid):
+                l,r,val = queries[i]
+                temp[l] += val
+                temp[r+1] -= val
             for i in range(len(nums)):
-                nums[i] += prefix[i]
-                _max = max(nums[i],_max)
-        return j
+                if i > 0:
+                    temp[i] += temp[i-1]
+                if temp[i] < nums[i]:
+                    return False
+            return True
+        ans = -1
+        while l <= r:
+            mid = (l + r)//2
+            if valid(mid):
+                ans = mid
+                r = mid - 1 
+            else:
+                l = mid + 1
+        return ans
+            
+
+
+
         
