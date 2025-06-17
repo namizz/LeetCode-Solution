ce8bfd9e74dad7db71fbc428eaddd492d24103bb
Naomi Zerfu <139370392+namizz@users.noreply.github.com>
2025-06-17 13:33:30 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1765-map-of-highest-peak/1765-map-of-highest-peak.py
+++ 1765-map-of-highest-peak/1765-map-of-highest-peak.py
@@ -1,35 +1,28 @@
 class Solution:
     def highestPeak(self, isWater: List[List[int]]) -> List[List[int]]:
-        # land and water
-        # isWater = [[]]
-        # 0->land 1->water
-        # return height[[]]
-        # height >= 0
-        # water height = 0
-        # two adjacent must have an absolute height difference of <= 1
-        # [1,2,0]
-        # [0,1,2]
-        # [1,2,3]
-        queue = deque()
         row = len(isWater)
         col = len(isWater[0])
-        height = [[-1 for _ in range(col)] for _ in range(row)]
+        queue = deque()
+        height = [[0]*row for _ in range(col)]
+        visited = set()
         for r in range(row):
             for c in range(col):
-                if isWater[r][c] == 1:
-                    height[r][c] = 0
+                if isWater[r][c]:
+                    visited.add((r,c))
                     queue.append((r,c))
-        print(queue)
-        direction = [(0,-1),(0,1),(1,0),(-1,0)]
+        direction = [(-1,0),(0,-1),(1,0),(0,1)]
         while queue:
-            cr, cc = queue.popleft()
+            r,c = queue.popleft()
             for dr, dc in direction:
-                r = dr+cr
-                c = cc+dc
-                if 0 <= r < row and 0<= c < col and height[r][c] == -1:
-                    height[r][c] = height[cr][cc] + 1
-                    queue.append((r,c))
-        return height
+                rr = r+dr
+                cc = c+dc
+                if (rr,cc) not in visited and( 0 <= rr < row and 0 <= cc < col):
+                    height[rr][cc] = height[r][c] + 1
+                    visited.add((rr,cc))
+                    queue.append((rr,cc))
+        return height 
 
+
+        
 
         
