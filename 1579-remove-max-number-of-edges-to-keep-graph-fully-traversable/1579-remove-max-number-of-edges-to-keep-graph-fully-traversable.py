a31476bea6c29789fe2fdcec20d1eec7ee8f1946
Naomi Zerfu <139370392+namizz@users.noreply.github.com>
2025-06-28 00:13:37 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.py
@@ -0,0 +1,46 @@
+class Solution:
+    def maxNumEdgesToRemove(self, n: int, edges: List[List[int]]) -> int:
+        # goal: to make the graph to be traverse through alice and bob
+        # return int: number_removable
+        # return -1: alice or bob can travese through at least one node
+        # Alice Graph:- contain edges that belongs to Alice 
+        # Bob Graph:- contains edge that belong to Bob
+        # ignore repetable edges and edges that connects with same root node
+        Alice = [i for i in range(n+1)]
+        Bob = [i for i in range(n+1)]
+        edges.sort(key=lambda x:x[0], reverse=True)
+        def find(graph, x):
+            if x != graph[x]:
+                graph[x] = find(graph,graph[x])
+            return graph[x]
+        ans = 0 # answer would be the removable edges
+        def union(graph, x, y):
+            rootx = find(graph, x)
+            rooty = find(graph, y)
+            if rootx != rooty:
+                graph[y] = rootx
+                return True
+            return False
+        for t,u,v in edges:
+            if t == 1:
+                if not union(Alice, u,v):
+                    ans += 1
+            elif t == 2:
+                if not union(Bob, u,v):
+                    ans += 1
+            else:
+                if not (union(Alice, u,v) and union(Bob, u,v)):
+                    ans += 1
+
+        rootAlice = set()
+        rootBob = set()
+        for i in range(1, n+1):
+            rootAlice.add(find(Alice,i))
+            rootBob.add(find(Bob,i))
+        return ans if len(rootAlice)+len(rootBob) == 2 else -1
+            
+
+
+        
+
+        
