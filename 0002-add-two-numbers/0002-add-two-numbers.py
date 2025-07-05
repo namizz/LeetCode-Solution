19ea5ae1330f8af8fe2c5f2a9a8558a81e38e216
Naomi Zerfu <139370392+namizz@users.noreply.github.com>
2025-07-05 19:40:16 +0300

    Time: 11 ms (10.23%), Space: 18.1 MB (14.79%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,36 +5,37 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        # head = l2
-        # l2.val += l1.val + rem if l2.val > 10:
-        # l2.val %10
-        # rem = l2.val//10
-        # if rem
-        
         dummy = ListNode("None")
         curr = dummy
-        rem = 0
-        while l1 or l2:
-            temp = ListNode(0)
-            if l1:
-                temp.val += l1.val
-                l1 = l1.next
-            if l2:
-                temp.val += l2.val
-                l2 = l2.next
-            temp.val += rem
-            added = temp.val 
-
-            if added > 9:
-                temp.val = added%10
-                rem = added//10
-            else:
-                rem = 0
-            curr.next = temp
+        r = 0
+        while l1 and l2:
+            k = l1.val + l2.val + r
+            r = k//10
+            k %= 10
+            curr.next = ListNode(k)
             curr = curr.next
-
-
-        if rem:
-            curr.next = ListNode(rem)
+            l1 = l1.next
+            l2 = l2.next
+        while l1:
+            k = l1.val + r
+            r = k//10
+            k %= 10
+            curr.next = ListNode(k)
+            curr = curr.next
+            l1 = l1.next
+        while l2:
+            k = l2.val + r
+            r = k//10
+            k %= 10
+            curr.next = ListNode(k)
+            curr = curr.next
+            l2 = l2.next
+        if r:
+            curr.next = ListNode(r)
         return dummy.next
             
+
+            
+
+
+        
