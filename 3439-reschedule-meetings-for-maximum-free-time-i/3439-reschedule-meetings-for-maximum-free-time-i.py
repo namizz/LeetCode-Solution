dac51e0482219f0b5c87d63a23723d7e793783ae
Naomi Zerfu <139370392+namizz@users.noreply.github.com>
2025-07-09 10:03:21 +0300

    Time: 71 ms (38.02%), Space: 36.3 MB (58.17%) - LeetHub

--- 3439-reschedule-meetings-for-maximum-free-time-i/3439-reschedule-meetings-for-maximum-free-time-i.py
+++ 3439-reschedule-meetings-for-maximum-free-time-i/3439-reschedule-meetings-for-maximum-free-time-i.py
@@ -4,12 +4,9 @@ class Solution:
         total = [0] * (len(startTime)+1)
         for i in range(len(startTime)):
             total[i+1] = total[i] + endTime[i] - startTime[i]
-        print(total)
         for i in range(k-1, len(startTime)):
             r = eventTime if i == len(startTime)-1 else startTime[i+1]
             l = 0 if i == k-1 else endTime[i-k]
-            print("rl", r,l)
             ans = max(ans, r-l-(total[i+1]-total[i-k+1]))
-            print(ans)
         return ans
         
