7467f8b1439634f15d432485aa02aff69a242224
Naomi Zerfu <139370392+namizz@users.noreply.github.com>
2025-10-07 22:17:28 +0300

    Time: 41 ms (32.46%), Space: 18.9 MB (8.81%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.py
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.py
@@ -7,33 +7,31 @@ class Solution:
             cmax = max(arr)
             _max = max(cmax, _max)
         direction = [(1,0), (0,1), (-1,0),(0,-1)]
-        def answer(value):
-            visited = set()
-            def findpath(r,c):
-                if (r,c) == (len(grid)-1, len(grid[0])-1):
-                    return True
-                
-                visited.add((r,c))
-                for dr, dc in direction:
-                    rr = dr + r
-                    cc = dc + c
-                    if 0 <= rr < len(grid) and 0 <= cc < len(grid[0]) and (rr,cc) not in visited:
-                        if grid[rr][cc] <= value:
-                            if findpath(rr,cc):
-                                return True
-                return False
-            return grid[0][0] <= value and findpath(0,0)
+    
+        def findpath(visited, value, r,c):
+            if (r,c) == (len(grid)-1, len(grid[0])-1):
+                return True
+            
+            visited.add((r,c))
+            for dr, dc in direction:
+                rr = dr + r
+                cc = dc + c
+                if 0 <= rr < len(grid) and 0 <= cc < len(grid[0]) and (rr,cc) not in visited:
+                    if grid[rr][cc] <= value:
+                        if findpath(visited, value, rr,cc):
+                            return True
+            return False
+      
         
         l = grid[0][0]
         r = _max
         ans = float(inf)
         while l <= r:
             mid = (l+r)//2
-            if answer(mid):
+            if findpath(set(), mid, 0,0):
                 ans = mid
                 r = mid - 1
             else:
                 l = mid + 1
-            print(ans, l, r)
         return ans 
         
