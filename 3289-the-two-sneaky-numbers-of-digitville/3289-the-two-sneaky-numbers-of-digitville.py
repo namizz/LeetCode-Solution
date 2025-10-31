1731e7e33ec19db8b2f5a35a674b7e39a1eb90b3
Naomi Zerfu <139370392+namizz@users.noreply.github.com>
2025-10-31 11:12:54 +0300

    Time: 4 ms (27.35%), Space: 18 MB (20.87%) - LeetHub

--- 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.py
+++ 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.py
@@ -1,9 +1,9 @@
 class Solution:
     def getSneakyNumbers(self, nums: List[int]) -> List[int]:
         hashmap = Counter(nums)
-        ans = []
+        res = []
         for k,v in hashmap.items():
             if v == 2:
-                ans.append(k)
-        return ans
+                res.append(k)
+        return res
         
