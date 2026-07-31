3221cba15a90f686eba217c6ca1ce92a6661ad87
Naomi Zerfu <139370392+namizz@users.noreply.github.com>
2026-07-31 11:30:28 +0300

    [3016] [Time Beats: N/A] [Memory Beats: N/A] - LeetPush

--- 3016-Minimum-Number-of-Pushes-to-Type-Word-II.py
+++ 3016-Minimum-Number-of-Pushes-to-Type-Word-II.py
@@ -1,21 +1,12 @@
 class Solution:
     def minimumPushes(self, word: str) -> int:
-        hashmap = Counter(word)
-        arr = []
-        for i in hashmap:
-            arr.append(hashmap[i])
-        arr.sort()
+        letter = Counter(word)
+        sh = sorted(letter.items(),key=lambda x:x[1], reverse=True)
         count = 0
         ans = 0
-        for i in range(len(arr)-1,-1,-1):
-            if len(arr)-1-i < 8:
-                ans += arr[i]
-            elif len(arr)-1-i < 16:
-                ans += (arr[i]*2)
-            elif len(arr) - 1 - i < 24:
-                ans += (arr[i]*3)
-            else:
-                ans += (arr[i]*4)
+        for _,b in sh:
+            ans += (count//8 + 1)*b
+            count += 1
         return ans
 
         
