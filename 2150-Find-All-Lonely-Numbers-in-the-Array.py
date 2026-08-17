f8860d3baab27c6778670ae17477c6716efc9d37
Naomi Zerfu <139370392+namizz@users.noreply.github.com>
2026-08-17 09:54:40 +0300

    [2150] [Time Beats: 40.38%] [Memory Beats: 67.38%] - LeetPush

--- 2150-Find-All-Lonely-Numbers-in-the-Array.py
+++ 2150-Find-All-Lonely-Numbers-in-the-Array.py
@@ -1,17 +1,10 @@
 class Solution:
     def findLonely(self, nums: List[int]) -> List[int]:
-        contain = set()
+        ans = []
         hashmap = Counter(nums)
         for i in range(len(nums)):
-            if hashmap[nums[i]] > 1:
-                contain.add(nums[i])
-            if nums[i] - 1 in hashmap:
-                contain.add(nums[i] - 1)
-            if nums[i] + 1 in hashmap:
-                contain.add(nums[i] + 1)
-        ans = []
-        for i in range(len(nums)):
-            if nums[i] not in contain:
-                ans.append(nums[i])
+            if hashmap[nums[i]] > 1 or nums[i] - 1 in hashmap or nums[i] + 1 in hashmap:
+                continue
+            ans.append(nums[i])
         return ans
         
